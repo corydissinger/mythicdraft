5864e77a5064a8076e5e2ff596194a78045e79e4
corydissinger <cory.dissinger@gmail.com>
2015-06-18 12:31:53 -0400

    Spring java-only config, lets go postgres

--- mythicdraftapp/src/main/java/com/cd/mythicdraft/controller/HomeController.java
+++ mythicdraftapp/src/main/java/com/cd/mythicdraft/controller/HomeController.java
@@ -2,16 +2,26 @@ package com.cd.mythicdraft.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cd.mythicdraft.model.Draft;
+import com.cd.mythicdraft.service.DraftService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+	@Autowired
+	private DraftService draftService;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String getHome(HttpServletRequest request) {
+		Draft draft = new Draft();
+		
+		draftService.addDraft(draft);
 		return "home";
 	}
 	
