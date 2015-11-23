6e7dc7fd68b4d818c1323f27cab6917a17d11756
corydissinger <cory.dissinger@gmail.com>
2015-11-23 16:01:27 -0500

    bad logic, 0 does not == 1

--- mythicdraftapp/src/main/java/com/cd/mythicdraft/model/IgnorableCards.java
+++ mythicdraftapp/src/main/java/com/cd/mythicdraft/model/IgnorableCards.java
@@ -4,11 +4,11 @@ import org.apache.commons.lang3.ArrayUtils;
 
 
 public enum IgnorableCards {
-	PLAINS("Plains", new String [] {""}),
-	ISLAND("Island", new String [] {""}),
-	SWAMP("Swamp", new String [] {""}),
-	MOUNTAIN("Mountain", new String [] {""}),
-	FOREST("Forest", new String [] {""});
+	PLAINS("Plains", new String [0]),
+	ISLAND("Island", new String [0]),
+	SWAMP("Swamp", new String [0]),
+	MOUNTAIN("Mountain", new String [0]),
+	FOREST("Forest", new String [0]);
 	
 	private IgnorableCards(final String name, final String [] setCodes) {
 		this.name = name;
@@ -20,7 +20,7 @@ public enum IgnorableCards {
 	
 	public static boolean isIgnorable(final String cardName, final String setCode) {
 		for(IgnorableCards ignorable : values()) {
-			if(ignorable.getName().equalsIgnoreCase(cardName)) {
+			if(ignorable.getName().equalsIgnoreCase(cardName.trim())) {
 				if(ignorable.getSetCodes().length == 0 || ArrayUtils.contains(ignorable.getSetCodes(), setCode)) {
 					return true;					
 				}
